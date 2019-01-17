4197998134f7cef86a4cb8927f5e55dadff0736b
Mathew Paret <mathewparet@gmail.com>
2019-01-17 14:57:05 +0530

    Fix missing reference

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use App\Rules\IsHuman;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
