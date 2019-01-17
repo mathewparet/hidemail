7e1f416d2010d59d11f35b3763ce2e54c45873b6
Mathew Paret <mathewparet@gmail.com>
2019-01-17 07:36:27 +0530

    Production build

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/landing.scss', 'public/css')
-   .copy('node_modules/startbootstrap-landing-page/img/bg-masterhed.jpg','public/img')
    .extract()
    .version();
