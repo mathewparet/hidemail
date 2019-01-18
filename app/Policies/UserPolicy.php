67081fe63b6be25d6d04ef97a3e932da85a3abe0
Mathew Paret <mathewparet@gmail.com>
2019-01-18 15:46:10 +0530

    Define user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -11,11 +11,40 @@ class UserPolicy
 
     public function before(User $user, $ability)
     {
+        if(in_array($ability, [
+            'index',
+            'forceVerify',
+            'toggleSuspension'
+        ]))
+            return null;
+
         return $user->id === 1 ? : null;
     }
 
     public function changeEmail(User $user, User $model)
     {
         return $user->id === $model->id;
+    }
+
+    public function update(User $user, User $model)
+    {
+        return $user->id === $model->id;
+    }
+
+    public function toggleSuspension(User $user, User $model)
+    {
+        return $user->id === 1
+            && $model->id != 1;
+    }
+
+    public function forceVerify(User $user, User $model)
+    {
+        return $user->id === 1
+            && $model->id == 1;
+    }
+
+    public function index(User $user)
+    {
+        return $user->id == 1;
     }
 }
