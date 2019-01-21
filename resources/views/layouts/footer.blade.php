2d3f5cb1e84488c871e71e38ff48dc987eaaa5ff
Mathew Paret <mathewparet@gmail.com>
2019-01-21 15:21:11 +0530

    Remove version disaply

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,4 @@
 <div class="footer">
     This service is provided as-is. All hidden email IDs are stored with AES-256 encryption. We never share your email ID with anyone without your consent.
     <br>&copy; <a href="{{$owner->link}}">Mathew Paret</a> 2019. All Rights Reserved.
-    <br>{{mathewparet\LaravelAppVersion\AppVersion::get()}}
 </div>
